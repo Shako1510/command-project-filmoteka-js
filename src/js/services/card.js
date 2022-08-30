e6dce0b3165bf3dcff6215c6622fcc685854cd1c
Bohdan Ses <bohdan.ses@gmail.com>
2022-08-31 00:02:45 +0200

    add library

--- src/js/services/card.js
+++ src/js/services/card.js
@@ -1,58 +1,37 @@
 import MoviesApiService from './moviesAPIService';
 const moviesApiService = new MoviesApiService();
-// console.log(moviesApiService);
 import { PaginationHandler } from './paginationHandler';
-console.log(PaginationHandler);
 document.addEventListener('DOMContentLoaded', onTrending);
-// async function getGenres() {
-//   const genres = await moviesApiService.fetchGenres();
-//   // console.log(genres);
-// }
-// getGenres();
-function onTrending(e) {
-  e.preventDefault();
+
+function onTrending() {
   moviesApiService.fetchTrendingMovies().then(appendGallery);
 }
+
 const gallery = document.querySelector('.collection__list');
-// const refs = {
-//   searchForm: document.querySelector('#search-form'),
-//   inputEl: document.querySelector('.form-input'),
-// };
-// refs.searchForm.addEventListener('submit', onSearchMovie);
-// // pagination.addEventListener('pageChanged', getPage);
-// let currentPage = 1;
-let currentPage = 1;
+
 let currentQuery = '';
-function getPage(pageNumber) {
-  console.log('getPage');
-  currentPage = pageNumber;
-  console.log(currentPage);
-  onSearchMovie();
-}
 
 export function getQuery(query) {
   currentQuery = query;
-  console.log('getQuery', currentQuery);
-  onSearchMovie(currentQuery);
+  onSearchMovie();
 }
-function onSearchMovie() {
+
+function onSearchMovie(page = 1) {
   clearContainer();
-  //  moviesApiService.resetPage();
-  moviesApiService
-    .fetchMoviesByQuery(currentQuery, currentPage)
-    .then(appendGallery);
+  moviesApiService.fetchMoviesByQuery(currentQuery, page).then(appendGallery);
 }
-export default function appendGallery(data) {
-  console.log(data);
+
+export function appendGallery(data) {
   clearContainer();
   const markupOneCard = data.results
     .map(movie => {
       return `
             <li class="collection__item">
-            <div class="card" id="${movie.id}">
+            <div class="card">
             <a href="" class="card__link" >
-          <img class=" card__img" src="https://www.themoviedb.org/t/p/w500/${movie.poster_path
-        }" alt="" ></a>
+          <img class=" card__img" src="https://www.themoviedb.org/t/p/w500/${
+            movie.poster_path
+          }" alt="" ></a>
           <div class="card__wrap">
              <h2 class="card__title" >${movie.title}</h2>
           <div class="card__data">
@@ -64,21 +43,27 @@ export default function appendGallery(data) {
         <p class="card__year card__text">|${movie.release_date.slice(0, 4)} </p>
               </div>
     </div>
+    
             </div>
-            </li>
+            </li>  
      `;
     })
     .join('');
   gallery.insertAdjacentHTML('beforeend', markupOneCard);
+
+  const pagination = new PaginationHandler(
+    data.page,
+    data.totalPage,
+    document.querySelector('.pagination__root')
+  );
+
+  pagination.initPagination();
+
+  // adding listener of choosing page by pagination
+
+  pagination.addEventListener('pageChanged', onSearchMovie);
 }
+
 function clearContainer() {
   gallery.innerHTML = '';
 }
-const pagination = new PaginationHandler(
-  15,
-  document.querySelector('.pagination__root')
-);
-pagination.initPagination();
-// adding listener of choosing page by pagination
-pagination.addEventListener('pageChanged', getPage);
-console.log(getPage(5));
--- src/library.js
+++ src/library.js
@@ -2,21 +2,21 @@ import btnLibraryHandler from './js/services/buttonLibraryHandler'
 
 
 
-renderInit()
-btnLibraryHandler()
+// renderInit()
+// btnLibraryHandler()
 
-function renderInit () {
+// function renderInit () {
 
-        const watchedFilms = JSON.parse(localStorage.getItem('watched')) 
-        if (!watchedFilms) {
-            // renderDefault()
-            console.log('отображаем заглушку')
-        }
-       else {
-    //  functionRender(watchedFilms)
-    console.log('рендерим watchedFilms', watchedFilms)
-        }
-}
+//         const watchedFilms = JSON.parse(localStorage.getItem('watched')) 
+//         if (!watchedFilms) {
+//             // renderDefault()
+//             console.log('отображаем заглушку')
+//         }
+//        else {
+//     //  functionRender(watchedFilms)
+//     console.log('рендерим watchedFilms', watchedFilms)
+//         }
+// }
 
 
 
