e88d45b40a61c732685d599accdaf9056144abc0
Shako1510 <93245492+Shako1510@users.noreply.github.com>
2022-08-30 21:05:45 +0000

    Deploying to gh-pages from @ Shako1510/command-project-filmoteka-js@eb925243cabbddd9c78e17a94e3a66931889cbe9 🚀

--- index.722fc994.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".modal"),t=document.querySelector(".footer-text-underline"),c=document.querySelector(".close");t.addEventListener("click",(function(){e.classList.add("is-active"),e.classList.remove("is-hidden")})),c.addEventListener("click",(function(){e.classList.remove("is-active"),e.classList.add("is-hidden")}));
-//# sourceMappingURL=index.722fc994.js.map
--- index.ce8ee1d2.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".modal"),t=document.querySelector(".footer-text-underline"),c=document.querySelector(".close");t.addEventListener("click",(function(){e.classList.add("is-active"),e.classList.remove("is-hidden")})),c.addEventListener("click",(function(){e.classList.remove("is-active"),e.classList.add("is-hidden")}))}();
-//# sourceMappingURL=index.ce8ee1d2.js.map
