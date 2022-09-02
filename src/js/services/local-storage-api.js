d5d636a8f63b24553c72dbff26b08519d6fda1df
VKlapan <vladimir.klapan@gmail.com>
2022-09-02 14:43:41 +0300

    fix add film to local - add removing the movie from current movie's storageItem

--- src/js/services/local-storage-api.js
+++ src/js/services/local-storage-api.js
@@ -27,12 +27,14 @@ export default class LocalStorageAPI {
       page,
       totalPages,
     };
-    console.log(result);
     return result;
   }
 
   // Додавання фільмів до колекції. Додає новий елемент до поточної колекції фільмів у сховищі
   setMovie(key, value) {
+    if (this.checkMovie(value.id) === this.changeKey(key))
+      this.removeMovie(this.changeKey(key), value.id);
+
     try {
       const currentCollection = this.getMovies(key).movies;
       currentCollection.push(value);
@@ -87,5 +89,9 @@ export default class LocalStorageAPI {
       isMovieInLocalStorage = 'queue';
     }
     return isMovieInLocalStorage;
+  }
+
+  changeKey(key) {
+    return key === 'watched' ? 'queue' : 'watched';
   }
 }
